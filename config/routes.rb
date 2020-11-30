c70464bf5d2321245a102094e7f87ea888be056f
doosemavis <moosedavis2011@gmail.com>
2020-11-30 16:51:08 -0600

    devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :instruments
-  resources :categories
-  resources :users
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20201130223016_create_instruments.rb
+++
@@ -1,8 +0,0 @@
-class CreateInstruments < ActiveRecord::Migration[6.0]
-  def change
-    create_table :instruments do |t|
-
-      t.timestamps
-    end
-  end
-end
