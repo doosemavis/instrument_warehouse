c5819bd230e97f8848832593e934a1268fbd30c2
doosemavis <moosedavis2011@gmail.com>
2020-12-01 17:01:48 -0600

    small changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {:registrations => "registrations", :omniauth_callbacks => "callbacks"}
   root to: 'application#welcome'
   
   resources :categories
   resources :instruments
   
-  devise_for :users, :controllers => {:registrations => "registrations", :omniauth_callbacks => "callbacks"}
   #change the route from /users/login to /login route
-  
   devise_scope :user do 
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
