2666694aa86288731f179abd4e9c686bf35702c7
doosemavis <moosedavis2011@gmail.com>
2020-12-01 11:43:19 -0600

    migrate categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_172634) do
+ActiveRecord::Schema.define(version: 2020_12_01_173809) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "instruments", force: :cascade do |t|
     t.string "name"
