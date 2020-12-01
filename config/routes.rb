48a73eec27de3222ebc7e817f3c52f7094301b5e
doosemavis <moosedavis2011@gmail.com>
2020-12-01 16:58:36 -0600

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ Rails.application.routes.draw do
   
   devise_scope :user do 
     get 'login', to: 'devise/sessions#new'
-  end 
-
-  devise_scope :user do 
     get 'signup', to: 'devise/registrations#new'
   end 
 end
