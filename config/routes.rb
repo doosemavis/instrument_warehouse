2dfc016a04ab93836662102adef25c03c0593e7c
doosemavis <moosedavis2011@gmail.com>
2020-12-01 17:02:37 -0600

    defined route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   #change the route from /users/login to /login route
   devise_scope :user do 
-    get 'login', to: 'devise/sessions#new'
-    get 'signup', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new', as: 'login'
+    get 'signup', to: 'devise/registrations#new', as: 'signup'
   end 
 end
