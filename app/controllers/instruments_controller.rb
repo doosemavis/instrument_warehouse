421c014681014d936c5ab66c79ca2b949d541c17
doosemavis <moosedavis2011@gmail.com>
2020-12-02 13:39:20 -0600

    instrument controller actions/methods

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -4,12 +4,28 @@ class InstrumentsController < ApplicationController
         @instruments = Instruments.all
     end 
 
+    def show
+    end 
 
+    def new
+    end 
+
+    def create
+    end 
+
+    def edit 
+    end 
+
+    def update
+    end 
+
+    def delete
+    end 
 
 
     private 
 
     def instrument_params
-        params.require(:instrument).permit(:name, :price, :weight, :description, :user_id, :category_id, :category_attributes[:name] )
+        params.require(:instrument).permit(:name, :price, :weight, :description, :user_id, :category_id, :category_attributes [:name])
     end 
 end
