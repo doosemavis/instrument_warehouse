37cc6dda31ccfcae720dd352e81febab7ba6bb39
doosemavis <moosedavis2011@gmail.com>
2020-12-02 13:37:51 -0600

    more nested_attributes

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -3,4 +3,13 @@ class InstrumentsController < ApplicationController
     def index 
         @instruments = Instruments.all
     end 
+
+
+
+
+    private 
+
+    def instrument_params
+        params.require(:instrument).permit(:name, :price, :weight, :description, :user_id, :category_id, :category_attributes[:name] )
+    end 
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,5 +1,5 @@
 class Instrument < ApplicationRecord
     belongs_to :user
     belongs_to :category
-    accepts_nested_attributes_for :category
+    accepts_nested_attributes_for :category, reject_if: :all_blank
 end
