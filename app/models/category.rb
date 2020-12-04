6ce00725689a3313c5936636d4716ebe5bd5c463
doosemavis <moosedavis2011@gmail.com>
2020-12-04 09:12:16 -0600

    validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
     has_many :instruments
-    has_many :users, through: :instruments 
+    has_many :users, through: :instruments
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
  
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,12 +1,14 @@
 class Instrument < ApplicationRecord
     belongs_to :user
     belongs_to :category
+    validates :name, :price, :weight, :description, presence: true
+    validates :name, :description, uniqueness: true
+
     # accepts_nested_attributes_for :category, reject_if: :all_blank
 
 
     def category_attributes=(attribute_hash)
         category = Category.find_or_create_by(attribute_hash)
         self.category = category
-        # binding.pry
     end 
 end
